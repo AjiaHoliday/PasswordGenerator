ebbb74b5907acb53e96020269328b6b8dff1166e
ajia <ajialeeholiday@gmail.com>
2021-08-18 21:53:40 -0600

    fixed parameters function

--- Develop/script.js
+++ Develop/script.js
@@ -9,51 +9,47 @@ var abcLower = lower.split("");
 var special = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
 var specialChar = special.split("");
 
-// variable declarations
-var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumbers;
-var confirmUpperCase;
-var confirmLowerCase;
+//additional functions
+var confirmLength = function(){ 
+  var passwordLength = window.prompt("How many characters (between 8-128) would you like your password to contain?");
+
+  if(passwordLength <= 7 || passwordLength >= 129 || isNaN(passwordLength)){
+    alert("Password length must be a number between 8-128 characters\nTRY AGAIN");
+    return confirmLength();
+  }
+  alert('Your password will have '+passwordLength+ ' characters.')
+};
+
+var parameters = function (){
+  var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters in your password.");
+  var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters in your password.");
+  var confirmNumbers = confirm("Click OK to confirm if you would like to include numbers in your password.");
+  var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters in your password.");
+  // WHEN I answer each prompt
+  // THEN my input should be validated and at least one character type should be selected
+  while(
+    !confirmLowerCase && 
+    !confirmUpperCase && 
+    !confirmNumbers &&
+    !confirmSpecialCharacter) {
+      alert("You must choose at least one parameter!");
+      return parameters();
+    }
+    alert("Your options:\nLowercase: " + confirmLowerCase 
+  +"\nUppercase: " + confirmUpperCase
+  +"\nInclude numbers: " + confirmNumbers
+  +"\nInclude special characters: " + confirmSpecialCharacter)
+};
 
 //Generate Password
 function generatePassword() {
 window.alert("I'm happy to help you generate a new password, I just have a few questions to get you the perfect password for your needs!");
-
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
-var confirmLength = prompt("How many characters (between 8-128) would you like your password to contain?");
-
-  if(confirmLength <= 7 || confirmLength >= 129 || isNaN(confirmLength)){
-    alert("Password length must be a number between 8-128 characters\nTRY AGAIN");
-  }
-  alert('Your password will have '+confirmLength+ ' characters.');
+confirmLength();
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-function parameters() {
-var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters in your password.");
-var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters in your password.");
-var confirmNumbers = confirm("Click OK to confirm if you would like to include numbers in your password.");
-var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters in your password.");
-
-while(confirmLowerCase === false && 
-  confirmUpperCase === false && 
-  confirmSpecialCharacter === false && 
-  confirmNumbers === false) {
-    alert("You must choose at least one parameter!");
-}
-parameters()
-
-  
-    }
-
-
-
-
-
-
-
-
+parameters();
 }
 
 
@@ -82,8 +78,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+
 
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
